eafcc0b0cbb8031986fe31c952300a7d7400bd1f
Igor Wiedler <igor@wiedler.ch>
2010-07-02 22:19:48 +0200

    add garbage collector script

---
+++ garbage_collector.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of trashbin.
+ *
+ * (c) 2010 Igor Wiedler
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require 'bootstrap.php';
+
+$q = Doctrine_Query::create()
+	->delete('Paste p')
+	->where('p.created_at < ?', date('Y-m-d H:i:s', strtotime('4 seconds ago')));
+$count = $q->execute();
+echo "$count records deleted\n";
