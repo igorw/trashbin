619d4218188253a146d956cf505e795810ae62a7
Igor Wiedler <igor@wiedler.ch>
2012-05-24 16:32:22 +0200

    Update to latest composer

--- src/app.php
+++ src/app.php
@@ -8,7 +8,7 @@
  * with this source code in the file LICENSE.
  */
 
-require_once __DIR__.'/../vendor/.composer/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 use Silex\Application;
 use Silex\Extension\TwigExtension;
