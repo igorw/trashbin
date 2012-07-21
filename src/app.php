a21e2a194d07af4848d5a5bdaea8e0259d53342c
Igor Wiedler <igor@wiedler.ch>
2012-07-21 15:03:22 +0200

    rm copyright statements

--- src/app.php
+++ src/app.php
@@ -1,12 +1,4 @@
 <?php
-/*
- * This file is part of trashbin.
- *
- * (c) 2010 Igor Wiedler
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
 
 require_once __DIR__.'/../vendor/autoload.php';
 
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,12 +1,4 @@
 <?php
-/*
- * This file is part of trashbin.
- *
- * (c) 2010 Igor Wiedler
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
 
 use Igorw\Trashbin\Storage;
 use Igorw\Trashbin\Validator;
