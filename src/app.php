3e6e9b99969e473aca57bee10278da7f529adaf4
Igor Wiedler <igor@wiedler.ch>
2013-10-17 16:24:27 +0200

    Handle autoloading in front controller

--- src/app.php
+++ src/app.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
-
 use Silex\Application;
 
 use Symfony\Component\HttpFoundation\Request;
--- web/index.php
+++ web/index.php
@@ -1,4 +1,6 @@
 <?php
 
+require_once __DIR__.'/../vendor/autoload.php';
+
 $app = require __DIR__.'/../src/app.php';
 $app->run();
